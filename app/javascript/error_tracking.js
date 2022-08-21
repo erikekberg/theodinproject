8c6a1f63a9b96573e979fce96be52c45b7fa6303
erikekberg <erik.oakmountain@gmail.com>
2022-08-21 21:39:14 +0200

    spelling error

--- app/javascript/error_tracking.js
+++ app/javascript/error_tracking.js
@@ -1,20 +1,17 @@
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 
-const {
-  SENTRY_DSN, currentUserLoggedIn, currentUserId, currentUserEmail,
-} = window;
+const { SENTRY_DSN, currentUserLoggedIn, currentUserId, currentUserEmail } =
+  window;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  environment: 'production',
-  allowUrls: [
-    /https?:\/\/(www\.)?theodinproject\.com/,
-  ],
+  environment: "production",
+  allowUrls: [/https?:\/\/(www\.)?theodinproject\.com/],
   ignoreErrors: [
-    'Possible side-effect in debug-evaluate',
-    'Unexpected end of input',
-    'Invalid or unexpected token',
-    'missing ) after argument list',
+    "Possible side-effect in debug-evaluate",
+    "Unexpected end of input",
+    "Invalid or unexpected token",
+    "missing ) after argument list",
   ],
 });
 
